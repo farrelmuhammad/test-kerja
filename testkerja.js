e354411fe3104c4272c0d8029ba9d84be061e0f7
Farrel Muhammad <81765631+farrelmuhammad@users.noreply.github.com>
2022-03-25 16:56:44 +0700

    update

--- testkerja.js
+++ testkerja.js
@@ -1,7 +1,12 @@
-const arr = [1, 3, 5, 7, 9, 11, 13]
+const arr = [1, 3, 5, 7, 9, 10, 11, 12]
 
 function gapCheck (arr) {
-  return arr[1] - arr[0]
+  const gap = []
+  for (i = 1; i < arr.length; i++) {
+    gap.push(arr[i] - arr[i-1])
+  }
+  
+  return gap
 }
 
 console.log(gapCheck(arr))
